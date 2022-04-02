868f5f5f4abb298af6422c4e65af8344b2924c1b
Polina Kasich <poli.key@yandex.ru>
2022-04-02 15:39:18 +0500

    Stage 2: the dot

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,24 @@
+#define MATH_3D_IMPLEMENTATION
+#define MATH_3D_HEADER
+#include <iostream>
+#include <GL/glew.h>
 #include <GL/freeglut.h> //GLUT - OpenGL Utility Library
+#include <glm/glm.hpp>		//#include "math_3d.h" - vector
+
+GLuint VBO;
 
 void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT); //clearing the frame buffer using the color specified above
+	
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 1);
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers(); //swap the background buffer and the frame buffer
+
 }
 
 int main(int argc, char** argv)
@@ -20,10 +35,26 @@ int main(int argc, char** argv)
 
 	glClearColor(1.0f, 0.0f, 1.0f, 0.0f); //setting the color
 
+
+	//initialize GLEW and check for errors
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	glm::vec3 Vertices[1];
+	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+
+
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+
 	glutMainLoop(); //transferring conrol to GLUT, it's waiting for events in the window
 
 	return 0;
 }
-
-
-
