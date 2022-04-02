b601d6bd19baf3b3b91750a8e680b14acbf38197
Polina Kasich <poli.key@yandex.ru>
2022-04-02 15:50:12 +0500

    Stage 3: triangle

--- Source.cpp
+++ Source.cpp
@@ -14,17 +14,18 @@ void RenderSceneCB()
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_POINTS, 0, 1);
+	//glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(0);
 
 	glutSwapBuffers(); //swap the background buffer and the frame buffer
 
 }
 
+
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv); //initialize glut
-
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //glut setup
 
 	glutInitWindowSize(1024, 768); //window parameters
@@ -32,7 +33,6 @@ int main(int argc, char** argv)
 	glutCreateWindow("Tutorial 01"); //title of the window
 
 	glutDisplayFunc(RenderSceneCB); //work in the window system
-
 	glClearColor(1.0f, 0.0f, 1.0f, 0.0f); //setting the color
 
 
@@ -44,14 +44,16 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	glm::vec3 Vertices[1];
-	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+	//glm::vec3 Vertices[1];
+	//Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
 
+	glm::vec3 Vertices[3];
+	Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = glm::vec3(1.0f, -1.0f, 0.0f);
+	Vertices[2] = glm::vec3(0.0f, 1.0f, 0.0f);
 
 	glGenBuffers(1, &VBO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 
 	glutMainLoop(); //transferring conrol to GLUT, it's waiting for events in the window
